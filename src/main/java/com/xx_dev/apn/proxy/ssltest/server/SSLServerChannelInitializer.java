7aa1bbabba30de68b0d14f522797d0ee8e9ba02b
xmxsuperstar <xmxsuperstar@gmail.com>
2013-04-02 11:01:50 +0800

    ssl test

---
+++ src/main/java/com/xx_dev/apn/proxy/ssltest/server/SSLServerChannelInitializer.java
@@ -0,0 +1,31 @@
+package com.xx_dev.apn.proxy.ssltest.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.ssl.SslHandler;
+
+import javax.net.ssl.SSLEngine;
+
+import com.xx_dev.apn.proxy.common.ApSSLContextFactory;
+
+/**
+ * @author xmx
+ * @version $Id: ApOutsideChannelInitializer.java,v 0.1 Feb 11, 2013 11:15:01 PM xmx Exp $
+ */
+public class SSLServerChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    public void initChannel(SocketChannel channel) throws Exception {
+        ChannelPipeline pipeline = channel.pipeline();
+
+        SSLEngine engine = ApSSLContextFactory.getSSLContext().createSSLEngine();
+        engine.setUseClientMode(false);
+        engine.setNeedClientAuth(true);
+        pipeline.addLast("ssl", new SslHandler(engine));
+
+        pipeline.addLast("handler", new SSLServerHandler());
+
+    }
+
+}
