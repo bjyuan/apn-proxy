dab71c8a7955d66a0bf963977507d141080d50ae
xmxsuperstar <xmxsuperstar@gmail.com>
2013-04-02 13:46:11 +0800

    jmx

--- inside_server_startup.sh
+++ inside_server_startup.sh
@@ -5,4 +5,4 @@ kill -9 $pid
 git pull --rebase
 mvn clean
 mvn compile
-mvn exec:java -Dexec.mainClass="com.xx_dev.apn.proxy.inside.ApInsideLauncher" &
+mvn exec:java -Dexec.mainClass="com.xx_dev.apn.proxy.inside.ApInsideLauncher" -Dcom.sun.management.jmxremote.port=8701 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false &
