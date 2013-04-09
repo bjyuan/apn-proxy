9e54042aa78b62036c16373f0fa0eb2004e94830
xmxsuperstar <xmxsuperstar@gmail.com>
2013-04-09 19:30:16 +0800

    run in background

--- test_server_startup.sh
+++ test_server_startup.sh
@@ -9,4 +9,4 @@ git pull --rebase
 mvn clean
 mvn test-compile
 #export MAVEN_OPTS="-Djava.rmi.server.hostname= -Dcom.sun.management.jmxremote.port=8701 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
-mvn exec:java -Dexec.mainClass="com.xx_dev.apn.proxy.test.HttpServerLauncher" -Dexec.classpathScope="test"
+mvn exec:java -Dexec.mainClass="com.xx_dev.apn.proxy.test.HttpServerLauncher" -Dexec.classpathScope="test" &
