eeffe5812a12c089f07f5be41811498939ca0edb
xmxsuperstar <xmxsuperstar@gmail.com>
2013-05-18 00:38:46 +0800

    fix

--- src/main/java/com/xx_dev/apn/proxy/IdleHandler.java
+++ src/main/java/com/xx_dev/apn/proxy/IdleHandler.java
@@ -3,13 +3,15 @@ package com.xx_dev.apn.proxy;
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
-import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
 
 public class IdleHandler extends ChannelDuplexHandler {
 
+    // private static Logger logger = Logger.getLogger(IdleHandler.class);
+
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-        if (evt instanceof IdleState) {
+        if (evt instanceof IdleStateEvent) {
             ctx.channel().close();
         }
     }
