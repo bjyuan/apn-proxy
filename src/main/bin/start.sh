91c9eed7f91234f09ef6e80171b419e4da8fa5ad
xmxsuperstar <xmxsuperstar@gmail.com>
2013-06-15 21:29:05 +0800

    test

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -8,4 +8,8 @@ for jar in `ls lib/*.jar`
 do
 	jars="$jars:""$jar"
 done
-java -XX:MaxDirectMemorySize=256m -Xmx256m -cp $jars com.xx_dev.apn.proxy.ApnProxyServerLauncher %*  1>/dev/null 2>&1 & echo $! > pid
+java -XX:MaxDirectMemorySize=256m -Xmx256m \
+        -Dcom.sun.management.jmxremote \
+        -Dcom.sun.management.jmxremote.port=8710 -Dcom.sun.management.jmxremote.authenticate=false \
+        -Dcom.sun.management.jmxremote.ssl=false \
+        -cp $jars com.xx_dev.apn.proxy.ApnProxyServerLauncher %*  1>/dev/null 2>&1 & echo $! > pid
