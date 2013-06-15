55cca312d5ed7b1b03c1d1d9334f590bfa0f75b3
xmxsuperstar <xmxsuperstar@gmail.com>
2013-06-16 01:17:21 +0800

    rm jmx connection config in start.sh

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -8,8 +8,4 @@ for jar in `ls lib/*.jar`
 do
 	jars="$jars:""$jar"
 done
-java -XX:MaxDirectMemorySize=256m -Xmx256m \
-        -Dcom.sun.management.jmxremote \
-        -Dcom.sun.management.jmxremote.port=8710 -Dcom.sun.management.jmxremote.authenticate=false \
-        -Dcom.sun.management.jmxremote.ssl=false \
-        -cp $jars com.xx_dev.apn.proxy.ApnProxyServerLauncher %*  1>/dev/null 2>&1 & echo $! > pid
+java -cp $jars com.xx_dev.apn.proxy.ApnProxyServerLauncher %*  1>/dev/null 2>&1 & echo $! > pid
