59cac57acf029fca05f692aa7dd4e194c7cc41a5
xmxsuperstar <xmxsuperstar@gmail.com>
2013-10-11 00:30:08 +0800

    add test case

--- src/test/java/com/xx_dev/apn/proxy/test/TestProxyBaidu.java
+++ src/test/java/com/xx_dev/apn/proxy/test/TestProxyBaidu.java
@@ -20,53 +20,13 @@ import org.junit.Test;
  * Date: 13-10-10
  * Time: PM3:58
  */
-public class TestProxyBaidu {
+public class TestProxyBaidu extends TestProxyBase{
 
     private static final Logger logger = Logger.getLogger(TestProxyBaidu.class);
 
-    @BeforeClass
-    public static void setUpServer() {
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                ApnProxyServerLauncher.main(null);
-            }
-        });
 
-        t.start();
+    @Override
+    protected String getTestHost() {
+        return "www.baidu.com";
     }
-
-    @Test
-    public void test() {
-        EventLoopGroup group = new NioEventLoopGroup();
-        try {
-            Bootstrap b = new Bootstrap();
-            b.group(group)
-                    .channel(NioSocketChannel.class)
-                    .handler(new TestHttpClientChannelInitializer());
-
-            // Make the connection attempt.
-            Channel ch = b.connect("127.0.0.1", 8700).sync().channel();
-
-            // Prepare the HTTP request.
-            HttpRequest request = new DefaultHttpRequest(
-                    HttpVersion.HTTP_1_1, HttpMethod.GET, "/");
-            request.headers().set(HttpHeaders.Names.HOST, "www.baidu.com");
-            request.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.CLOSE);
-            //request.headers().set(HttpHeaders.Names.ACCEPT_ENCODING, HttpHeaders.Values.GZIP);
-
-
-            // Send the HTTP request.
-            ch.writeAndFlush(request);
-
-            // Wait for the server to close the connection.
-            ch.closeFuture().sync();
-        } catch (InterruptedException e) {
-            logger.error(e.getMessage(), e);
-        } finally {
-            // Shut down executor threads to exit.
-            group.shutdownGracefully();
-        }
-    }
-
 }
---
+++ src/test/java/com/xx_dev/apn/proxy/test/TestProxyBase.java
@@ -0,0 +1,76 @@
+package com.xx_dev.apn.proxy.test;
+
+import com.xx_dev.apn.proxy.ApnProxyServerLauncher;
+import com.xx_dev.apn.proxy.ApnProxyXmlConfig;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.DefaultHttpRequest;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpVersion;
+import org.apache.log4j.Logger;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xjx
+ * Date: 13-10-11
+ * Time: 上午12:07
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class TestProxyBase {
+
+    private static final Logger logger = Logger.getLogger(TestProxyBase.class);
+
+    @BeforeClass
+    public static void setUpServer() {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ApnProxyServerLauncher.main(null);
+            }
+        });
+
+        t.start();
+    }
+
+    @Test
+    public void test() {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new TestHttpClientChannelInitializer());
+
+            // Make the connection attempt.
+            Channel ch = b.connect("127.0.0.1", ApnProxyXmlConfig.getConfig().getPort()).sync().channel();
+
+            // Prepare the HTTP request.
+            HttpRequest request = new DefaultHttpRequest(
+                    HttpVersion.HTTP_1_1, HttpMethod.GET, "http://"+getTestHost()+"/");
+            request.headers().set(HttpHeaders.Names.HOST, getTestHost());
+            request.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.CLOSE);
+            //request.headers().set(HttpHeaders.Names.ACCEPT_ENCODING, HttpHeaders.Values.GZIP);
+
+
+            // Send the HTTP request.
+            ch.writeAndFlush(request);
+
+            // Wait for the server to close the connection.
+            ch.closeFuture().sync();
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage(), e);
+        } finally {
+            // Shut down executor threads to exit.
+            group.shutdownGracefully();
+        }
+    }
+
+    protected abstract String getTestHost();
+}
