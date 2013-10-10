bce1cc09a5895ba344756e1fcc1040d577afc428
xmxsuperstar <xmxsuperstar@gmail.com>
2013-10-10 21:42:25 +0800

    test

--- src/test/java/com/xx_dev/apn/proxy/test/TwitterTest.java
+++ src/test/java/com/xx_dev/apn/proxy/test/TwitterTest.java
@@ -1,6 +1,9 @@
 package com.xx_dev.apn.proxy.test;
 
+import com.xx_dev.apn.proxy.ApnProxyServerLauncher;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * User: xmx
@@ -9,11 +12,19 @@ import org.junit.Before;
  */
 public class TwitterTest {
 
-    @Before
-    public void setUp() {
+    @BeforeClass
+    public static void setUpServer() {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //ApnProxyServerLauncher.main(null);
+            }
+        });
 
+        t.start();
     }
 
+    @Test
     public void test() {
 
     }
