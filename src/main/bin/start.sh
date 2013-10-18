51f886f0011c2c5c6e28fef944a10841a8c4d8cc
xmxsuperstar <xmxsuperstar@gmail.com>
2013-10-18 13:45:51 +0800

    let script sleep 2s

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-pid=`ps aux | grep "com.xx_dev.apn.proxy.ApnProxyServerLauncher" | grep java | awk '{print $2}' | sort | head -2`
+pid=`ps aux | grep "com.xx_dev.apn.proxy.ApnProxyServerLauncher" | grep java | awk '{print $2}' | sort | head -1`
 echo $pid
 kill $pid
+
+sleep 2
 
 for jar in `ls lib/*.jar`
 do
 	jars="$jars:""$jar"
 done
-java $JAVA_OPTS -cp $jars com.xx_dev.apn.proxy.ApnProxyServerLauncher 1>/dev/null 2>&1 & echo $! > pid
+java $JAVA_OPTS -cp $jars com.xx_dev.apn.proxy.ApnProxyServerLauncher 1>/dev/null 2>&1
