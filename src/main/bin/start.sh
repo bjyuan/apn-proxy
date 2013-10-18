cd42304c8066b47ede14efc3b4ec75ea6a720c57
xmxsuperstar <xmxsuperstar@gmail.com>
2013-10-18 13:36:21 +0800

    better pid find

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pid=`ps aux | grep "com.xx_dev.apn.proxy.ApnProxyServerLauncher" | awk '{print $2}' | sort | head -1`
+pid=`ps aux | grep "com.xx_dev.apn.proxy.ApnProxyServerLauncher" | grep java | awk '{print $2}' | sort | head -2`
 echo $pid
 kill $pid
 
--- src/main/bin/stop.sh
+++ src/main/bin/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pid=`ps aux | grep "com.xx_dev.apn.proxy.ApnProxyServerLauncher" | awk '{print $2}' | sort | head -1`
+pid=`ps aux | grep "com.xx_dev.apn.proxy.ApnProxyServerLauncher" | grep java | awk '{print $2}' | sort | head -2`
 kill $pid
